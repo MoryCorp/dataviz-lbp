0a5027a67b0b776a2b937b499354a4056caab58b
jabert.sichaib <jabert.sichaib@lecole-ldlc.com>
2018-10-26 14:58:34 +0200

    price regular to price happy hour

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,11 @@ var markers = [];
 
 function initMap() {
 
+    $('#select-day').on('change', function(e){
+        console.log(this.value);
+        setMarkesHours(this.value,7)
+    });
+
     function setMapOnAll(map) {
         for (var i = 0; i < markers.length; i++) {
             markers[i].setMap(map);
@@ -17,39 +22,72 @@ function initMap() {
         clearMarkers();
         markers = [];
     }
-    function setMarkesHours(hour){
-        console.log(data);
+    function setMarkesHours(jour,hour){
+
+        //console.log(data);
         for (i = 0; i < data.length; i++) { //TODO : Mettre la condition pour le range slider !! Il faut deja reformate le sheet
-            if (parseFloat(data[i].price_regular) >= average_expensive) {
-                beer_icon = "assets/img/beer-expensive.png";
-                // console.log(data[i].price_regular)
-            } else if (parseFloat(data[i].price_regular) <= average_cheap) {
-                beer_icon = "assets/img/beer-cheap.png";
-                // console.log(data[i].price_regular)
-            } else {
-                beer_icon = "assets/img/beer-regular.png";
-            }
-
-            marker = new google.maps.Marker({
-                position: new google.maps.LatLng(data[i].lat, data[i].long),
-                map: map,
-                icon: new google.maps.MarkerImage(beer_icon)
-            });
-
-            markers.push(marker);
-
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                var content_bar = '<h1>' + data[i].bar_name + '</h1><p>' + data[i].address + '</p>';
-
-
-                return function() {
-                    infowindow.setContent(content_bar);
-
-                    infowindow.open(map, marker);
+            if (jour === "Lundi"){
+                if (data[i].lundi.length > 0){
+                    console.log(data[i])
                 }
-            })
-            (marker, i));
+            }
+            else if (jour === "Mardi"){
+                console.log(data[i])
+            }
+            else if (jour === "Mercredi"){
+                console.log(data[i])
+            }
+            else if (jour === "Jeudi"){
+                console.log(data[i])
+            }
+            else if (jour === "Vendredi"){
+                console.log(data[i])
+            }
+            else if (jour === "Samedi"){
+                console.log(data[i])
+            }
+            else if (jour === "Dimanche"){
+                console.log(data[i])
+            }
+            else if (jour === "Tous les jours"){
+                console.log(data[i])
+            }
         }
+
+
+            /*
+                    for (i = 0; i < data.length; i++) { //TODO : Mettre la condition pour le range slider !! Il faut deja reformate le sheet
+                        if (parseFloat(data[i].price_regular) >= average_expensive) {
+                            beer_icon = "assets/img/beer-expensive.png";
+                            // console.log(data[i].price_regular)
+                        } else if (parseFloat(data[i].price_regular) <= average_cheap) {
+                            beer_icon = "assets/img/beer-cheap.png";
+                            // console.log(data[i].price_regular)
+                        } else {
+                            beer_icon = "assets/img/beer-regular.png";
+                        }
+
+                        marker = new google.maps.Marker({
+                            position: new google.maps.LatLng(data[i].lat, data[i].long),
+                            map: map,
+                            icon: new google.maps.MarkerImage(beer_icon)
+                        });
+
+                        markers.push(marker);
+
+                        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                            var content_bar = '<h1>' + data[i].bar_name + '</h1><p>' + data[i].address + '</p>';
+
+
+                            return function() {
+                                infowindow.setContent(content_bar);
+
+                                infowindow.open(map, marker);
+                            }
+                        })
+                        (marker, i));
+                    }
+            */
 
     }
 
@@ -71,15 +109,15 @@ function initMap() {
 
   $("#PrixM").text('Prix Moyen : ' + Math.round(average_price * 100) / 100 + "€");
 
-  // console.log("Prix moyenne basse " + average_cheap);
-  // console.log("Prix Moyen : " + average_price);
-  // console.log("Prix moyenne haute : " + average_expensive);
+//  console.log("Prix Moyen : " + average_price);
+//  console.log("Prix moyenne basse " + average_cheap);
+//  console.log("Prix moyenne haute : " + average_expensive);
 
   for (i = 0; i < data.length; i++) {
-    if (parseFloat(data[i].price_regular) >= average_expensive) {
+    if (parseFloat(data[i].price_happy_hour) >= average_expensive) {
       beer_icon = "assets/img/beer-expensive.png";
       // console.log(data[i].price_regular)
-    } else if (parseFloat(data[i].price_regular) <= average_cheap) {
+    } else if (parseFloat(data[i].price_happy_hour) <= average_cheap) {
       beer_icon = "assets/img/beer-cheap.png";
       // console.log(data[i].price_regular)
     } else {
@@ -110,7 +148,7 @@ function initMap() {
 
     $( "#target" ).click(function() {
         //deleteMarkers()
-        //setMarkesHours()
+        setMarkesHours('lundi',17) //TODO Il faut que l'input du jours soit format comme ça => lundi_happy, mardi_hayppy...
     });
 }
 
@@ -119,7 +157,6 @@ $(function() {
   $.get("assets/js/gmap_style.json", function(result) {
     map_style = result;
 
-    // console.log("READY");
 
     var URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2gr_tgGs7ziW83sgOauf6WZw7sMfE4ToemXeffMue1JMdmjSaDcu5flbKga-h9DtU0YkCS_lrPQTf/pub?gid=1788698224&single=true&output=csv";
 
@@ -128,16 +165,15 @@ $(function() {
       var cpt_nb_bar = 0;
       data = d;
 
-      //     console.log(data);
       //   console.log(data);
       data.forEach(function(d) {
-        if (d.lat !== "#N/A") {
-          if (d.price_regular.length >= 1) {
-            average_price += parseFloat(d.price_regular);
+          if (d.price_happy_hour.length >= 1) {
+//            console.log(d.price_happy_hour);
+            average_price += parseFloat(d.price_happy_hour);
             cpt_nb_bar++;
-            //console.log(average_pricerice)
+           // console.log(cpt_nb_bar)
           }
-        }
+
       });
       average_price = average_price / cpt_nb_bar;
       //TODO : Ici c'est trop bien
