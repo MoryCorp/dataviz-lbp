8ddce1ef3dea2ea863e9d30ea93f53a7442c7d2b
MoryCorp <amaury.cleuziou@gmail.com>
2018-10-26 09:28:46 +0200

    First commit to change map style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,21 @@
 
 var data;
+var map_style;
+
+$.getJSON( "./gmap_style.json", function( data ) {
+  map_style = [data];
+  console.log(map_style);
+  });
+
 
 function initMap() {
 
    // console.log(data);
 
-    var locations = [
-        ['Bondi Beach', -33.890542, 151.274856, 4],
-        ['Coogee Beach', -33.923036, 151.259052, 5],
-        ['Cronulla Beach', -34.028249, 151.157507, 3],
-        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-        ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
-
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: new google.maps.LatLng(45.7623323, 4.8262804,15),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
     });
 
 
