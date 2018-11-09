1881094a691c0e60d2a90b81553dd223820400b6
jabert.sichaib <jabert.sichaib@lecole-ldlc.com>
2018-11-09 13:41:06 +0100

    slider pick hour for refreshing map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 var data;
+var data_nofilter;
 var map_style;
 var average_price = 0;
 var markers = [];
@@ -106,6 +107,7 @@ function initMap() {
         });
 
         markers.push(marker);
+    //
 
         google.maps.event.addListener(marker, 'click', (function (marker, i) {
 
@@ -128,13 +130,13 @@ function initMap() {
                 var timerH_happy = Math.abs(hour_happy - current_hour);
                 var timerM_happy = minute_happy - current_minute;
                 var happy_minute = hour_happy * 60 + minute_happy;
-
+                var content_bar = "";
                 if (!isNaN(happy_minute)) {
                     var date = new Date(null);
                     date.setMinutes((happy_minute - current_time_minute)); // specify value for SECONDS here
                     var timer_happy = date.toISOString().substr(11, 8);
+                    var content_bar = "Fin de l'happy hour dans : " + timer_happy[1] + "h" + timer_happy[3] + timer_happy[4] + "m";
                 }
-                var content_bar = "Fin de l'happy hour dans : " + timer_happy[1]+"h"+timer_happy[3]+timer_happy[4]+"m";
 
             }
             else {
@@ -158,13 +160,12 @@ function initMap() {
                     var content_bar = "Ce bar ne propose pas d'happy hour ou nos informations sont incomplètes";
                 }
                 else {
-                    var content_bar = "La prochaine happy hour commence dans : " + timer_happy[1]+"h"+timer_happy[3]+timer_happy[4]+"m";
+                    var content_bar = "La prochaine happy hour commence dans : " + timer_happy[1] + "h" + timer_happy[3] + timer_happy[4] + "m";
 
                 }
 
 
             }
-
 
 
             return function () {
@@ -173,13 +174,13 @@ function initMap() {
                 //infowindow.open(map, marker);
                 $("#bar_info").html("");
                 $("#bar_name").html(data[i].bar_name);
-                $("#current_price").html("Le prix de la pinte de bière est actuellement de : " +  data[i].current_price + "€");
+                $("#current_price").html("Le prix de la pinte de bière est actuellement de : " + data[i].current_price + "€");
                 $("#happy_hour").html(content_bar);
                 $("#goto_maps").html("<i class='fas fa-arrow-circle-right fa-3x' aria-hidden='true'></i>");
                 $("#goto_maps").attr('href', 'https://maps.google.com/?q=' + data[i].bar_name);
 
 
-                if (data[i].is_happy){
+                if (data[i].is_happy) {
                     $("#next_price").html("Le prochain prix sera de : " + data[i].price_regular + "€");
                 }
                 else {
@@ -217,6 +218,7 @@ function parse_timeinterval(s) {
 
 $(function () {
 
+
     $('[data-toggle="tooltip"]').tooltip();
 
 
@@ -253,17 +255,68 @@ $(function () {
                 }
             });
 
+            data_nofilter = data;
             // Filter only open bars
-            data = data.filter(function (d) {
+            data = data_nofilter.filter(function (d) {
                 return d.is_open;
             });
 
             average_price = average_price / cpt_nb_bar;
             //TODO : Ici c'est trop bien
-            initMap()
+            console.log(d);
+            initMap();
+            console.log(markers)
 
+
+
+            $("#toto").click(function () {
+                 cheap_beer = 100;
+                markers = [];
+                current_hour = 17;
+                current_minute = 30;
+                cpt_nb_bar = 0;
+                current_time_minute = current_hour * 60 + current_minute;
+                average_price = 0;
+                data = data_nofilter;
+                data.forEach(function (d) {
+
+                    ti = parse_timeinterval(d[days2label[current_day]]);
+                    ti_hh = parse_timeinterval(d[days2label[current_day] + "_happy"]);
+
+                    if (current_time_minute >= ti[0] && current_time_minute <= ti[1]) {
+                        d.is_open = true;
+                    } else {
+                        d.is_open = false;
+                    }
+                    if (current_time_minute >= ti_hh[0] && current_time_minute <= ti_hh[1]) {
+                        d.is_happy = true;
+                        d.current_price = parseFloat(d.price_happy_hour.replace(",", "."));
+
+                    } else {
+                        d.is_happy = false;
+                        d.current_price = parseFloat(d.price_regular.replace(",", "."));
+                    }
+                    if (!isNaN(d.current_price)) {
+                        average_price += d.current_price;
+                        cpt_nb_bar++;
+                    }
+                });
+
+                // Filter only open bars
+                data = data.filter(function (d) {
+                    return d.is_open;
+                });
+
+                average_price = average_price / cpt_nb_bar;
+                console.log(d);
+
+                initMap();
+                console.log(markers);
+                console.log(data_nofilter);
+            });
         });
 
 
     });
+
 });
